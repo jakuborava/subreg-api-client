8a7dd5de702803f99abe1c88a563757cfb7db913
Jakub Orava <Jakuborava@gmail.com>
2025-11-13 15:22:15 +0100

    Order history

---
+++ src/DataTransferObjects/OrderHistoryEntry.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jakuborava\SubregApiClient\DataTransferObjects;
+
+use Carbon\CarbonImmutable;
+
+readonly class OrderHistoryEntry
+{
+    public function __construct(
+        public CarbonImmutable $date,
+        public string $text,
+        public int $orderId,
+        public float $sum,
+        public float $credit
+    ) {
+    }
+
+    public static function fromSubregAPIResponse(array $data): OrderHistoryEntry
+    {
+        return new OrderHistoryEntry(
+            CarbonImmutable::parse($data['date']),
+            $data['text'],
+            (int)$data['order'],
+            (float)$data['sum'],
+            (float)$data['credit']
+        );
+    }
+}
--- src/Endpoints/Orders.php
+++ src/Endpoints/Orders.php
@@ -2,6 +2,9 @@
 
 namespace Jakuborava\SubregApiClient\Endpoints;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Collection;
+use Jakuborava\SubregApiClient\DataTransferObjects\OrderHistoryEntry;
 use Jakuborava\SubregApiClient\Exceptions\LoginFailedException;
 use Jakuborava\SubregApiClient\Exceptions\RequestFailedException;
 use Jakuborava\SubregApiClient\Responses\OrderResponse;
@@ -31,5 +34,21 @@ class Orders
         $response = (new SubregRequest)->call('Info_Order', ['data' => ['order' => $orderID]]);
 
         return OrderResponse::fromSubregAPIResponse($response['data']['order']);
+    }
+
+    /**
+     * @return Collection<int, OrderHistoryEntry>
+     * @throws LoginFailedException
+     * @throws RequestFailedException
+     */
+    public function history(CarbonImmutable $from, CarbonImmutable $to): Collection
+    {
+        $response = (new SubregRequest)->call(
+            'Get_Accountings',
+            ['data' => ['from' => $from->format('Y-m-d'), 'to' => $to->format('Y-m-d')]]
+        );
+
+        return (new Collection($response['data']['accounting']))
+            ->map(fn(array $order) => OrderHistoryEntry::fromSubregAPIResponse($order));
     }
 }
